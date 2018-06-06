6f8b63abbbf1ab77162ae22d5f37140cbea9cfa5
davitbek <davitzeynalyan1991@gmail.com>
2018-06-06 16:33:39 +0400

    Update ListComposer.php

--- src/ViewComposer/ListComposer.php
+++ src/ViewComposer/ListComposer.php
@@ -1,28 +1,68 @@
 <?php
+
 namespace LaraViewComposer\ViewComposer;
 
+use Illuminate\Http\Request;
 use Illuminate\View\View;
+use LaraRepo\Criteria\Where\WhereCriteria;
 
 class ListComposer
 {
     /**
-     * @var array
+     * @var
      */
     protected $repository;
+
+    /**
+     * @var null
+     */
+    protected $listable = null;
+
+    /**
+     * @var bool
+     */
+    protected $isParaneNull = false;
+
+    /**
+     * @var null
+     */
+    protected $status = true;
+
+    /**
+     * @var bool
+     */
+    protected $isFindCurrentId = false;
 
     /**
      * @param View $view
      */
     public function compose(View $view)
     {
-        if(!is_array($this->repository)) {
+        if (!is_array($this->repository)) {
             $this->repository = [$this->repository];
         }
-        
-        foreach($this->repository as $repository) {
-            $items = $repository->findList(true);
+
+        $this->listable = (array) $this->listable;
+        $this->status = (array) $this->status;
+        $this->isFindCurrentId = (array) $this->isFindCurrentId;
+        $this->isParentNull = (array) $this->isParentNull;
+
+        foreach ($this->repository as $index => $repository) {
+            $listable = (isset($this->listable[$index])) ? $this->listable[$index] : null;
+            $isParentNull = (isset($this->isParentNull[$index])) ? $this->isParentNull[$index] : false;
+            $status = (isset($this->status[$index])) ? $this->status[$index] : true;
+            $isFindCurrentId = (isset($this->isFindCurrentId[$index])) ? $this->isFindCurrentId[$index] : false;
+
+            if (!empty($isParentNull)) {
+                $repository->pushCriteria(new WhereCriteria('parent_id', null));
+            }
+
+            if (isset($view->getData()['item']->id) && !$isFindCurrentId) {
+                $repository->pushCriteria(new WhereCriteria('id', $view->getData()['item']->id, '!='));
+            }
+
+            $items = $repository->findList($status, $listable);
             $view->with($repository->getTable(), $items);
         }
     }
-
 }
